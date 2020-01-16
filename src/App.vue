cb06a3a81636a2adffa09916838cf08a3571e513
Kozlovtseva <alina.lm@mail.ru>
2020-01-16 10:35:26 +0300

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,30 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <Form />
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Form from "./components/Form.vue";
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+    name: "app",
+    components: {
+        Form
+    }
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: "Avenir", Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+    background-image: url("https://klondaik-plitka.ru/wp-content/uploads/2014/10/grey-marbled-paper1.jpg");
+    height: 100vh;
+    margin-top: 0;
 }
 </style>
---
+++ src/components/Form.vue
@@ -0,0 +1,12 @@
+<template lang="html">
+    <div class="container"></div>
+</template>
+
+<style lang="scss" scoped>
+.container {
+    background-image: url("../assets/back.jpg");
+    height: 100%;
+    margin: 0 auto;
+    width: 45vw;
+}
+</style>
